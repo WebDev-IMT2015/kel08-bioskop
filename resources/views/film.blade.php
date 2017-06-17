4348beefac0971acea3953a7a636047c57927f22
solapunch <solapunch@gmail.com>
2017-06-17 13:07:39 +0700

    Film / Add Film
    Film sam addfilm bedanya apa ?

--- resources/views/film.blade.php
+++ resources/views/film.blade.php
@@ -3,44 +3,52 @@
 <head>
   <title>Cinema XXI</title>
   <style>
-    #main
+    input[type=text], select {
+        width: 100%;
+        padding: 10px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+    input[type=number], select {
+        width: 100%;
+        padding: 10px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+    input[type=submit] {
+        width: 100%;
+        background-color: #4CAF50;
+        color: white;
+        padding: 10px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+    input[type=submit]:hover {
+        background-color: #45a049;
+    }
+    .panel-body
+    {
+        width: 40%
+        border-radius: 5px;
+        background-color: #f2f2f2;
+        padding: 10px 80px 10px 80px;
+        margin: 10px 35% 10px 35%;
+    }
+    h1
     {
       text-align: center;
-      color: white;
     }
     body
     {
       background-color: black;
-    }
-    div.form-group
-    {
-      text-align: center;
-      color: white;
-      margin-bottom: 12px;
-    }
-    #judul
-    {
-      width: 200px; /* Full-width */
-      font-size: 16px; /* Increase font-size */
-      padding: 6px 12px 6px 12px; /* Add some padding */
-      border: 1px solid #ddd; /* Add a grey border */
-      margin-bottom: 12px; /* Add some space below the input */
-    }
-    #durasi
-    {
-      width: 50px; /* Full-width */
-      font-size: 16px; /* Increase font-size */
-      padding: 6px 12px 6px 40px; /* Add some padding */
-      border: 1px solid #ddd; /* Add a grey border */
-      margin-bottom: 12px; /* Add some space below the input */
-    }
-    #rate
-    {
-      width: 50px; /* Full-width */
-      font-size: 16px; /* Increase font-size */
-      padding: 6px 12px 6px 40px; /* Add some padding */
-      border: 1px solid #ddd; /* Add a grey border */
-      margin-bottom: 12px; /* Add some space below the input */
     }
   </style>
 </head>
@@ -49,10 +57,10 @@
     <div class="row">
       <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
-          <div class="panel-heading" id="main"><h1>Tambah Film</h1></div>
           <div class="panel-body">
             <form class="form-horizontal" role="form" method="POST"">
-              <!-- {{ csrf_field() }} -->
+              {{ csrf_field() }}
+              <h1>Tambah Film</h1>
               <div class="form-group">
                 <label for="judulbox" class="col-md-4 control-label">Judul Film</label>
                 <div class="col-md-6">
@@ -68,12 +76,17 @@
               <div class="form-group">
                 <label for="rate" class="col-md-4 control-label">Rate Umur</label>
                 <div class="col-md-6">
-                  <input id="rate" type="text" name="rate_umur" required>
+                  <select id="rate" name="rate_umur" required>
+                    <option value="semua">Semua Umur</option>
+                    <option value="dewasa">Dewasa</option>
+                    <option value="remaja">Remaja</option>
+                    <option value="anak">Anak - anak</option>
+                  </select>
                 </div>
               </div>
               <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
-                  <button type="submit" class="btn btn-primary">Tambah Film</button>
+                  <input type="submit" value="Submit" placeholder="Tambah Film">
                 </div>
               </div>
             </form>
