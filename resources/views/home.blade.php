f307cf6df1ca6cbca3c15dabc756455d76250a9d
Kevin Hartarto <k.hartarto9@gmail.com>
2017-06-17 11:22:26 +0700

    Add hak akses setelah login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,29 +6,36 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Admin Panel</div>
-
-                <div class="panel-body">
-                    Hello {{ Auth::user()->name }} <br>
-                    Email anda : {{ Auth::user()->email }} <br>
-                    Jabatan : {{ Auth::user()->type}}
-                    <div class="content">
-                        <div class="title m-b-md">
-                            CINEMA XXI - ADMIN MODE
-                        </div>
-                        @if(Route::has('login'))
-                            @if(Auth::check())
-                                <div class="links">
-                                    <a href="{{ url('/film')}}">Film</a><br>
-                                    <a href="{{ url('/bioskop')}}">Boskop</a><br>
-                                    <a href="https://laravel-news.com">Studio</a><br>
-                                    <a href="https://forge.laravel.com">Jadwal</a><br>
-                                    <a href="https://github.com/laravel/laravel">Jam Tanyang</a><br>
-                                    <a href="https://github.com/laravel/laravel">Laporan Penjualan</a><br>
-                                </div>
+                @if(Auth::user()->type == 'admin')
+                    <div class="panel-body">
+                        Hello {{ Auth::user()->name }} <br>
+                        Email anda : {{ Auth::user()->email }} <br>
+                        Jabatan : {{ Auth::user()->type}}
+                        <div class="content">
+                            <div class="title m-b-md">
+                                CINEMA XXI - ADMIN MODE
+                            </div>
+                            @if(Route::has('login'))
+                                @if(Auth::check())
+                                    <div class="links">
+                                        <a href="{{ url('/film')}}">Film</a><br>
+                                        <a href="{{ url('/bioskop')}}">Boskop</a><br>
+                                        <a href="https://laravel-news.com">Studio</a><br>
+                                        <a href="https://forge.laravel.com">Jadwal</a><br>
+                                        <a href="https://github.com/laravel/laravel">Jam Tanyang</a><br>
+                                        <a href="https://github.com/laravel/laravel">Laporan Penjualan</a><br>
+                                    </div>
+                                @endif
                             @endif
-                        @endif
+                        </div>
                     </div>
-                </div>
+                @else
+                    <div class="panel-body">
+                        Hello {{ Auth::user()->name }} <br>
+                        Email anda : {{ Auth::user()->email }} <br>
+                        Jabatan : {{ Auth::user()->type}}
+                    </div>
+                @endif
             </div>
         </div>
     </div>
