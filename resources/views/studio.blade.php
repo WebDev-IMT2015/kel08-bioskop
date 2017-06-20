7e28655d0ef9a0969445165e35edd053edb27ef2
Kevin Hartarto <k.hartarto9@gmail.com>
2017-06-20 14:06:40 +0700

    dataPenjualan view route

--- resources/views/studio.blade.php
+++ resources/views/studio.blade.php
@@ -68,7 +68,7 @@
         <li class="active"><a href="{{ url('/studio')}}">Studio</a></li>
         <li><a href="{{ url('/jadwal')}}">Jadwal</a></li>
         <li><a href="{{ url('/jamtayang')}}">Jam Tayang</a></li>
-        <li><a href="{{ url('/datapenjualan')}}">Laporan Penjualan</a></li>
+        <li><a href="{{ url('/dataPenjualan')}}">Laporan Penjualan</a></li>
         <li><a href="{{ url('/user') }}">Users</a></li>
       </ul><br>
     </div>
